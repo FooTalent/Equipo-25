275a03802f33dc5f6f1e2b2894be85573b547190
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-10-04 21:56:26 -0500

    Modified userModel

--- back/src/models/userModel.js
+++ back/src/models/userModel.js
@@ -4,7 +4,11 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["admin", "superAdmin", "accountant"],
+    required: true,
+  },
   state: { type: Boolean, required: true },
 });
 
