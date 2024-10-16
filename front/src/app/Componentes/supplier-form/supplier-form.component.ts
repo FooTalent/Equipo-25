0e4af0baeca3332a6cb0db9555548b66329f339d
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-10-16 17:58:16 -0500

    Filing supplierForm.TS

--- front/src/app/Componentes/supplier-form/supplier-form.component.ts
+++ front/src/app/Componentes/supplier-form/supplier-form.component.ts
@@ -1,93 +1,77 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
+  ReactiveFormsModule,
   FormControl,
   FormGroup,
-  ReactiveFormsModule,
   Validators,
-  ValidationErrors,
-  AbstractControl,
-  FormBuilder,
 } from '@angular/forms';
-import { UsersService } from '../../services/users.service';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-/* import { FooterComponent } from '../footer/footer.component';
-import { HederComponent } from '../header/heder.component'; */
+//import { JwtHelperService } from "@auth0/angular-jwt";
+import { ToastrService } from 'ngx-toastr';
+import { LoginCredentials } from '../../interfaces/login-credentials';
+import { LoginService } from '../../services/login.service';
+import { FooterComponent } from '../footer/footer.component';
+import { HederComponent } from '../header/heder.component';
+
+//const jwtHelperService = new JwtHelperService();
 
 @Component({
   selector: 'app-supplier-form',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    HttpClientModule,
-    CommonModule /* , HederComponent, FooterComponent */,
-  ],
+  imports: [ReactiveFormsModule, HederComponent, FooterComponent],
   templateUrl: './supplier-form.component.html',
   styleUrl: './supplier-form.component.css',
 })
-export class SupplierFormComponent implements OnInit {
-  supplierRegister: FormGroup;
+export class SupplierFormComponent {
+  toastrService = inject(ToastrService);
+  loginService: LoginService = inject(LoginService);
 
-  constructor(private form: FormBuilder) {
-    this.supplierRegister = new FormGroup({
-      thirdPartyData: new FormControl('', [Validators.required]),
-      nitData: new FormControl('', [Validators.required]),
-      phoneData: new FormControl('', [Validators.required]),
-      emailData: new FormControl('', [Validators.required, Validators.email]),
-      cityData: new FormControl('', [Validators.required]),
-      departmentData: new FormControl('', [Validators.required]),
-    });
-  }
+  supplierData = new FormGroup({
+    thirdPartyData: new FormControl('', [Validators.required]),
+    nitData: new FormControl('', [Validators.required]),
+    phoneData: new FormControl('', [Validators.required]),
+    emailData: new FormControl('', [Validators.required, Validators.email]),
+    cityData: new FormControl('', [Validators.required]),
+    departmentData: new FormControl('', [Validators.required]),
+  });
 
-  ngOnInit(): void {}
+  handleLoginSubmit() {
+    if (this.supplierData.valid) {
+      const thirdPartyData = this.supplierData.value.thirdPartyData;
+      const nitData = this.supplierData.value.nitData;
+      const phoneData = this.supplierData.value.phoneData;
+      const emailData = this.supplierData.value.emailData;
+      const cityData = this.supplierData.value.cityData;
+      const departmentData = this.supplierData.value.departmentData;
 
-  /* // Alternar visibilidad de la contraseña
-  togglePasswordVisibility(): void {
-    this.showPassword = !this.showPassword;
-  } */
-
-  // Método para enviar el formulario
-  onSubmit() {
-    if (this.supplierRegister.valid) {
-      const { confirmPassword, ...formValue } = this.supplierRegister.value;
-      const dataToSend = { ...formValue, state: true };
-
-      if (this.user) {
-        // Actualizamos el usuario si ya existe (modo edición)
-        const userId = this.user._id;
-        this.usersService.updateUser(userId, dataToSend).subscribe({
-          next: (response) => {
-            alert('Usuario actualizado correctamente');
-            this.userEdited.emit(); // Emitimos evento de usuario editado
-          },
-          error: (error) => {
-            console.error('Error al actualizar el usuario', error);
-            alert('Ocurrió un error durante la actualización.');
-          },
-        });
-      } else {
-        // Creamos un nuevo usuario (modo creación)
-        this.usersService.createUser(dataToSend).subscribe({
-          next: (response) => {
-            alert('Usuario registrado correctamente');
-            this.supplierRegistered.emit(); // Emitimos evento de usuario registrado
-          },
-          error: (error) => {
-            console.error('Error al registrar el usuario', error);
-            alert('Ocurrió un error durante el registro.');
-          },
+      if (
+        typeof thirdPartyData === 'string' &&
+        typeof nitData === 'string' &&
+        typeof phoneData === 'string' &&
+        typeof emailData === 'string' &&
+        typeof cityData === 'string' &&
+        typeof departmentData === 'string'
+      ) {
+        const credentials: LoginCredentials = {
+          email,
+          password,
+        };
+        this.loginService.login(credentials).subscribe((res: any) => {
+          // any porque si no hacemos el Backend no sabemos qué vendrá de él
+          //console.log('Response: ', res);
+          //const decoded = jwtHelperService.decodeToken(res.data.token);
+          //console.log("decoded: ", decoded);
+          if (res.state === 'Successful') {
+            localStorage.setItem('token', res.data.token);
+            //this.router.navigateByUrl('/home'); // Redirigir Way_1
+          } else {
+            /* console.log("Invalid credentials") */
+            this.toastrService.error('Credenciales inválidas');
+          }
         });
       }
-    } else if (this.supplierRegister.errors?.['passwordsMismatch']) {
-      alert('Las contraseñas no coinciden');
-    } else if (
-      this.supplierRegister.get('password')?.errors?.['passwordStrength']
-    ) {
-      alert(
-        'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, un número y un carácter especial.'
-      );
     } else {
-      alert('Faltan campos por llenar.');
+      /* console.log("Empty form filds"); */
+      this.toastrService.warning('Campo de credenciales vacío');
     }
   }
 }
--- front/src/app/services/suppliers.service.ts
+++ front/src/app/services/suppliers.service.ts
@@ -1,34 +1,30 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+/* import { supplierCredentials } from '../interfaces/supplier-credentials';
+CREATE A INTERFACE with supplierData */
 @Injectable({
   providedIn: 'root',
 })
-
 export class SuppliersService {
-
+  constructor() {}
   httpClient = inject(HttpClient);
+  router = inject(Router);
+  toastrService = inject(ToastrService);
 
-  private readonly URL_PRODUCT = "http://localhost:3000/Supplier"
+  // BACKEND URL a donde se harán las peticiones (del login)
+  API_URL = 'http://localhost:3000/Supplier';
 
-  constructor() { }
-
-  createSupplier(supplierData: any) {
-    return this.httpClient.post(this.URL_PRODUCT, supplierData);
+  createSupplier(supplierData: any /* supplierCredentials */) {
+    return this.httpClient.post(this.API_URL, supplierData);
   }
 
   getSupplier() {
-    return this.httpClient.get(this.URL_PRODUCT);
+    return this.httpClient.get(this.API_URL);
   }
 
   deleteSupplierById(id: string) {
-    return this.httpClient.delete(`${this.URL_PRODUCT}/${id}`);
+    return this.httpClient.delete(`${this.API_URL}/${id}`);
   }
 }
-
-  
-
-
-  
-
