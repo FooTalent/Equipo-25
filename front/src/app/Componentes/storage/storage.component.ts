b2be8dfbda0b14d71a87c20f4cdc877944a7165c
David <david.santiagonino20@gmail.com>
2024-10-18 22:35:34 -0500

    storage diseño

--- front/src/app/Componentes/storage/storage.component.ts
+++ front/src/app/Componentes/storage/storage.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { StorageService } from '../../services/storage.service';
 import { DatePipe } from '@angular/common';
 import { HederComponent } from "../header/heder.component";
+import { FooterComponent } from "../footer/footer.component";
 @Component({
   selector: 'app-storage',
   standalone: true,
-  imports: [DatePipe, HederComponent],
+  imports: [DatePipe, HederComponent, FooterComponent],
   templateUrl: './storage.component.html',
   styleUrl: './storage.component.css',
 })
