adbf2741670aa5b31ed753133b1812c0810f8fe4
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-10-18 22:24:24 -0500

    Fixed footer ; Deleted footerAccountant-footerSuperAdmin components, node_modules, package and package-lock.json ; Filed app.routes.ts

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Componentes/home/home.component';
 import { LoginComponent } from './Componentes/login/login.component';
 import { StorageComponent } from './Componentes/storage/storage.component';
+import { FaqInfoComponent } from './Componentes/faq-info/faq-info.component';
 import { UserListComponent } from './superAdmin/user-list/user-list.component';
 
 export const routes: Routes = [
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'storage', component: StorageComponent },
   { path: 'user-list', component: UserListComponent },
+  { path: 'FAQ', component: FaqInfoComponent },
 ];
