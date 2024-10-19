809aa3d294476d7297dc6bfec65f0afb53f0972a
David <david.santiagonino20@gmail.com>
2024-10-18 21:02:33 -0500

    modificaciónes al storage

--- front/src/app/Componentes/storage/storage.component.ts
+++ front/src/app/Componentes/storage/storage.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { StorageService } from '../../services/storage.service';
 import { DatePipe } from '@angular/common';
+import { HederComponent } from "../header/heder.component";
 @Component({
   selector: 'app-storage',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, HederComponent],
   templateUrl: './storage.component.html',
   styleUrl: './storage.component.css',
 })
